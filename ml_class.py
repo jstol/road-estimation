6e40933ea032bd3be3165a4bcaf9601afe43f5f7
yuanwang1988 <yuan.wang688@gmail.com>
2015-11-22 20:12:50 -0500

    ml_class.py - implemented logistic reg

--- ml_class.py
+++ ml_class.py
@@ -4,8 +4,17 @@ from __future__ import absolute_import, print_function, unicode_literals
 """defines classes that encapsulates the sci-kit learn ML algorithms"""
 
 import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from sklearn.externals import joblib
+
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn import linear_model, decomposition, datasets
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+
 
 
 #ScikitLearnML Class:
@@ -33,6 +42,9 @@ class ScikitLearnML:
 		if (self.name == "knn"):
 			return knn_train(train_inputs, train_targets, self.hyperparameters_dic, model_name)
 
+		elif (self.name == "logistic"):
+			return logistic_train(train_inputs, train_targets, self.hyperparameters_dic, model_name)
+
 		else:
 			print("Error: unexpected ML algorithm name")
 
@@ -49,6 +61,9 @@ class ScikitLearnML:
 		if (self.name == "knn"):
 			return knn_predict(test_inputs, self.hyperparameters_dic, model_name)
 
+		elif (self.name == "logistic"):
+			return logistic_predict(test_inputs, self.hyperparameters_dic, model_name)
+
 		else:
 			print("Error: unexpected ML algorithm name")
 			return None
@@ -63,12 +78,16 @@ class ScikitLearnML:
 			- class_rate:	scalar rate of correct responses
 			- ce:			scalar cross entropy if the the cross entropy flag is true (appropriate if the test predictions are probabilities)
 		"""
-		targets = np.array(targets)
-		predictions = np.array(predictions)
+		targets = np.array(targets).ravel()
+		predictions = np.array(predictions).ravel()
 
 		# Cross entropy
 		if (cross_entropy_flag == True):
-			ce = -(np.dot(targets.T, np.log(predictions)) + np.dot((1-targets).T, np.log(1-predictions)))
+			predictions_t1 = 0.5*np.ones(predictions.shape)
+			predictions_t0 = 0.5*np.ones(predictions.shape)
+			predictions_t1[targets == 1] = predictions[targets == 1]
+			predictions_t0[targets == 0] = predictions[targets == 0]
+			ce = -(np.dot(targets, np.log(predictions_t1)) + np.dot((1-targets), np.log(1-predictions_t0)))/float(len(targets))
 		else:
 			ce = None
 
@@ -83,11 +102,16 @@ class ScikitLearnML:
 
 #K Nearest Neighbours
 def knn_train(train_inputs, train_targets, hyperparameters_dic, model_name):
+	train_inputs = np.array(train_inputs)
+	train_targets = np.array(train_targets)
 	np.savez(model_name, train_inputs = train_inputs, train_targets = train_targets)
 	return None
 
 def knn_predict(test_inputs, hyperparameters_dic, model_name):
-	#extract hyperparameters
+	#force test inputs to be np arrays:
+	test_inputs = np.array(test_inputs)
+
+	#unpack hyperparameters
 	k = hyperparameters_dic['k']
 
 	#load model
@@ -102,6 +126,44 @@ def knn_predict(test_inputs, hyperparameters_dic, model_name):
 	#make predictions
 	test_pred = np.array(neigh.predict_proba(test_inputs))
 	return test_pred[:, 1]
+
+
+#SVM
+
+
+#Logistic Regression
+def logistic_train(train_inputs, train_targets, hyperparameters_dic, model_name):
+	#force our train inputs and targets to be np arrays
+	train_inputs = np.array(train_inputs)
+	train_targets = np.array(train_targets)
+
+	#unpack hyperparameters:
+	penalty = hyperparameters_dic['penalty'] #str 'l1' or 'l2'
+	regularization_term = hyperparameters_dic['regularization_term'] #value
+
+	#fit model
+	logistic = linear_model.LogisticRegression(penalty= penalty, C=regularization_term)
+	logistic.fit(train_inputs, train_targets)
+
+	#save model
+	joblib.dump(logistic, (model_name+'.pkl'))
+
+def logistic_predict(test_inputs, hyperparameters_dic, model_name):
+	#force test inputs to be np arrays:
+	test_inputs = np.array(test_inputs)
+
+	#unpack hyperparameters:
+	penalty = hyperparameters_dic['penalty'] #str 'l1' or 'l2'
+	regularization_term = hyperparameters_dic['regularization_term'] #value
+
+	#load model
+	logistic = joblib.load((model_name+'.pkl'))
+
+	#make predictions
+	test_pred = np.array(logistic.predict_proba(test_inputs))
+	return test_pred[:, 1]
+
+
 
 
 #Test area
@@ -119,5 +181,17 @@ test_pred = knn_alg.predict(test_X, 'knn_test_model.npz')
 print(test_pred)
 
 ce, class_rate = knn_alg.evaluate(test_y, test_pred, cross_entropy_flag = True)
+print(ce)
+print(class_rate)
+
+
+print("Tests - logistic")
+
+logistic_alg = ScikitLearnML('logistic', {'penalty': 'l2', 'regularization_term':0.1})
+logistic_alg.train(train_X, train_y, 'logistic_test_model')
+test_pred = logistic_alg.predict(test_X, 'logistic_test_model')
+print(test_pred)
+
+ce, class_rate = logistic_alg.evaluate(test_y, test_pred, cross_entropy_flag = True)
 print(ce)
 print(class_rate)
