98682e58e308bf04f33e4c06f1b23352e9834d0e
Yuan Wang <yuan.wang688@gmail.com>
2015-12-16 22:43:12 -0500

    modified logistic.sh to add lower regularization

--- model_scripts/logistic.sh
+++ model_scripts/logistic.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 alg="logistic"
 params_set=( \
+	'{"penalty": "l1", "regularization_term": 0.0}' \
+	'{"penalty": "l1", "regularization_term": 0.000005}' \
+	'{"penalty": "l1", "regularization_term": 0.00005}' \
+	'{"penalty": "l1", "regularization_term": 0.0005}' \
 	'{"penalty": "l1", "regularization_term": 0.005}' \
 	'{"penalty": "l1", "regularization_term": 0.01}' \
 	'{"penalty": "l1", "regularization_term": 0.05}' \
@@ -12,6 +16,10 @@ params_set=( \
 	'{"penalty": "l1", "regularization_term": 10}' \
 	'{"penalty": "l1", "regularization_term": 20}' \
 	'{"penalty": "l1", "regularization_term": 50}' \
+	'{"penalty": "l2", "regularization_term": 0.0}' \
+	'{"penalty": "l2", "regularization_term": 0.000005}' \
+	'{"penalty": "l2", "regularization_term": 0.00005}' \
+	'{"penalty": "l2", "regularization_term": 0.0005}' \
 	'{"penalty": "l2", "regularization_term": 0.005}' \
 	'{"penalty": "l2", "regularization_term": 0.01}' \
 	'{"penalty": "l2", "regularization_term": 0.05}' \
