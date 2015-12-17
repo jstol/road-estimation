2d2225995f8714dd065f8fdb0f4cebbe916e3f20
Jake Stolee <jakestolee@gmail.com>
2015-12-16 23:08:42 -0500

    Decision tree script

---
+++ model_scripts/decision_tree.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+alg="decision_tree"
+params_set=( \
+	'{"criterion": "glini", "max_depth": null, "min_samples_split": 2}' \
+	'{"criterion": "glini", "max_depth": null, "min_samples_split": 5}' \
+	'{"criterion": "glini", "max_depth": null, "min_samples_split": 10}' \
+	'{"criterion": "glini", "max_depth": null, "min_samples_split": 20}' \
+	'{"criterion": "glini", "max_depth": null, "min_samples_split": 50}' \
+	'{"criterion": "glini", "max_depth": null, "min_samples_split": 100}' \
+	'{"criterion": "glini", "max_depth": 2, "min_samples_split": 2}' \
+	'{"criterion": "glini", "max_depth": 2, "min_samples_split": 5}' \
+	'{"criterion": "glini", "max_depth": 2, "min_samples_split": 10}' \
+	'{"criterion": "glini", "max_depth": 2, "min_samples_split": 20}' \
+	'{"criterion": "glini", "max_depth": 2, "min_samples_split": 50}' \
+	'{"criterion": "glini", "max_depth": 2, "min_samples_split": 100}' \
+	'{"criterion": "glini", "max_depth": 5, "min_samples_split": 2}' \
+	'{"criterion": "glini", "max_depth": 5, "min_samples_split": 5}' \
+	'{"criterion": "glini", "max_depth": 5, "min_samples_split": 10}' \
+	'{"criterion": "glini", "max_depth": 5, "min_samples_split": 20}' \
+	'{"criterion": "glini", "max_depth": 5, "min_samples_split": 50}' \
+	'{"criterion": "glini", "max_depth": 5, "min_samples_split": 100}' \
+	'{"criterion": "glini", "max_depth": 7, "min_samples_split": 2}' \
+	'{"criterion": "glini", "max_depth": 7, "min_samples_split": 5}' \
+	'{"criterion": "glini", "max_depth": 7, "min_samples_split": 10}' \
+	'{"criterion": "glini", "max_depth": 7, "min_samples_split": 20}' \
+	'{"criterion": "glini", "max_depth": 7, "min_samples_split": 50}' \
+	'{"criterion": "glini", "max_depth": 7, "min_samples_split": 100}' \
+	'{"criterion": "glini", "max_depth": 10, "min_samples_split": 2}' \
+	'{"criterion": "glini", "max_depth": 10, "min_samples_split": 5}' \
+	'{"criterion": "glini", "max_depth": 10, "min_samples_split": 10}' \
+	'{"criterion": "glini", "max_depth": 10, "min_samples_split": 20}' \
+	'{"criterion": "glini", "max_depth": 10, "min_samples_split": 50}' \
+	'{"criterion": "glini", "max_depth": 10, "min_samples_split": 100}' \
+	'{"criterion": "entropy", "max_depth": null, "min_samples_split": 2}' \
+	'{"criterion": "entropy", "max_depth": null, "min_samples_split": 5}' \
+	'{"criterion": "entropy", "max_depth": null, "min_samples_split": 10}' \
+	'{"criterion": "entropy", "max_depth": null, "min_samples_split": 20}' \
+	'{"criterion": "entropy", "max_depth": null, "min_samples_split": 50}' \
+	'{"criterion": "entropy", "max_depth": null, "min_samples_split": 100}' \
+	'{"criterion": "entropy", "max_depth": 2, "min_samples_split": 2}' \
+	'{"criterion": "entropy", "max_depth": 2, "min_samples_split": 5}' \
+	'{"criterion": "entropy", "max_depth": 2, "min_samples_split": 10}' \
+	'{"criterion": "entropy", "max_depth": 2, "min_samples_split": 20}' \
+	'{"criterion": "entropy", "max_depth": 2, "min_samples_split": 50}' \
+	'{"criterion": "entropy", "max_depth": 2, "min_samples_split": 100}' \
+	'{"criterion": "entropy", "max_depth": 5, "min_samples_split": 2}' \
+	'{"criterion": "entropy", "max_depth": 5, "min_samples_split": 5}' \
+	'{"criterion": "entropy", "max_depth": 5, "min_samples_split": 10}' \
+	'{"criterion": "entropy", "max_depth": 5, "min_samples_split": 20}' \
+	'{"criterion": "entropy", "max_depth": 5, "min_samples_split": 50}' \
+	'{"criterion": "entropy", "max_depth": 5, "min_samples_split": 100}' \
+	'{"criterion": "entropy", "max_depth": 7, "min_samples_split": 2}' \
+	'{"criterion": "entropy", "max_depth": 7, "min_samples_split": 5}' \
+	'{"criterion": "entropy", "max_depth": 7, "min_samples_split": 10}' \
+	'{"criterion": "entropy", "max_depth": 7, "min_samples_split": 20}' \
+	'{"criterion": "entropy", "max_depth": 7, "min_samples_split": 50}' \
+	'{"criterion": "entropy", "max_depth": 7, "min_samples_split": 100}' \
+	'{"criterion": "entropy", "max_depth": 10, "min_samples_split": 2}' \
+	'{"criterion": "entropy", "max_depth": 10, "min_samples_split": 5}' \
+	'{"criterion": "entropy", "max_depth": 10, "min_samples_split": 10}' \
+	'{"criterion": "entropy", "max_depth": 10, "min_samples_split": 20}' \
+	'{"criterion": "entropy", "max_depth": 10, "min_samples_split": 50}' \
+	'{"criterion": "entropy", "max_depth": 10, "min_samples_split": 100}' \
+)
+start_date=$(date +"%m-%d-%Y-%s")
+
+count=1
+for params in "${params_set[@]}"
+do
+	results_subdir="${start_date}/${count}"
+	./run_main.sh "${alg}" "${params}" "${results_subdir}" "${start_date}"
+
+	let count++
+done
+
+echo "====================================="
+echo "DONE"
+echo "====================================="
