bc864f0a28f57f23cf5ead5562702de99520bb06
Jake Stolee <jakestolee@gmail.com>
2015-12-21 15:08:44 -0500

    Fixed some bugs in final scripts

---
+++ final_main.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+"""Main script for training and evaluating models"""
+
+# Standard modules
+import argparse, time, os, json, csv
+# Third party modules
+import numpy as np
+import matplotlib.pyplot as plt
+# Model implementations
+from models import KNNModel, SVMModel, LogisticRegressionModel, NeuralNetworkModel, MOGModel, DecisionTreeModel, AdaboostModel, BaggingModel, RandomForestModel, ExtraTreesModel
+from sklearn.ensemble import ExtraTreesClassifier
+
+def create_dir_if_not_exists(filename):
+	if not os.path.exists(os.path.dirname(filename)):
+		os.makedirs(os.path.dirname(filename))
+
+def run_feature_importance():
+# Feature importance forest
+# Build a forest and compute the feature importances
+	print('Feature importance using random forest')
+
+	# TODO also look at RandomForestClassifier - what's the difference?
+	forest = ExtraTreesClassifier(n_estimators=250, random_state=0)
+
+	forest.fit(train_X, train_y)
+	importances = forest.feature_importances_
+	std = np.std([rand_tree.feature_importances_ for rand_tree in forest.estimators_], axis=0)
+	indices = np.argsort(importances)[::-1]
+
+	# Print the feature ranking
+	print("Feature ranking:")
+
+	for f in range(train_X.shape[1]):
+	    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+	# Plot the feature importances of the forest
+	plt.figure()
+	plt.title("Feature importances")
+	plt.bar(range(train_X.shape[1]), importances[indices],
+	       color="r", yerr=std[indices], align="center")
+	plt.xticks(range(train_X.shape[1]), indices)
+	plt.xlim([-1, train_X.shape[1]])
+	plt.show()
+
+# Global vars
+available_models = ['knn', 'logistic', 'svm', 'mog', 'neural_net', 'decision_tree', 'randomforest', 'extratrees']
+available_ensemble_methods = ['adaboost', 'bagging' , '']
+
+# Set up command-line arguments
+_argument_parser = argparse.ArgumentParser(
+	prog='main.py',
+	description='Main script for training and evaluating models.'
+)
+# Define arguments
+_argument_parser.add_argument('-m', '--model',
+	choices=available_models,
+	dest='model_name',
+	help='the model to run',
+	required=True
+)
+_argument_parser.add_argument('-e', '--ensemble-type',
+	choices=available_ensemble_methods,
+	dest='ensemble_method',
+	help='the ensemble method to use (if any)',
+	required=False,
+	default=None
+)
+_argument_parser.add_argument('-o', '--output',
+	dest='output_file',
+	help='the file to output results to',
+	required=True
+)
+_argument_parser.add_argument('-p', '--params',
+	dest='params',
+	help='a JSON string containing parameters for the specified algorithm',
+	required=True
+)
+_argument_parser.add_argument('--train-data',
+	dest='train_data_file',
+	help='the training data file to use',
+	required=True
+)
+_argument_parser.add_argument('--train-predictions-output',
+	dest='train_predictions_file',
+	help='the file to write the training predictions to',
+	required=True
+)
+_argument_parser.add_argument('--valid-data',
+	dest='valid_data_file',
+	help='the valid data file to use',
+	required=True
+)
+_argument_parser.add_argument('--valid-predictions-output',
+	dest='valid_predictions_file',
+	help='the file to write the valid predictions to',
+	required=True
+)
+_argument_parser.add_argument('--test-data',
+	dest='test_data_file',
+	help='the test data file to use',
+	required=True
+)
+_argument_parser.add_argument('--test-predictions-output',
+	dest='test_predictions_file',
+	help='the file to write the test predictions to',
+	required=True
+)
+_argument_parser.add_argument('--model-file',
+	dest='model_file',
+	help='the file to output the model to',
+	required=True
+)
+_argument_parser.add_argument('--summary-file',
+	dest='summary_file',
+	help='the summary report file to output all metrics to',
+	required=True
+)
+# TODO add arguments for file locations, etc.
+args = _argument_parser.parse_args()
+
+# Run script
+if __name__ == '__main__':
+	model_name = args.model_name
+	ensemble_method = args.ensemble_method
+	output_file = args.output_file
+	params_json = args.params
+	train_data_file = args.train_data_file
+	train_output_file = args.train_predictions_file
+	valid_data_file = args.valid_data_file
+	valid_output_file = args.valid_predictions_file
+	test_data_file = args.test_data_file
+	test_output_file = args.test_predictions_file
+	model_file = args.model_file
+	summary_file = args.summary_file
+
+	if not ensemble_method:
+		print("Running {0} - trained on {1}".format(model_name, train_data_file))
+	else:
+		print("Running {0} ({1}) - trained on {2}".format(ensemble_method, model_name, train_data_file))
+
+	#training set
+	train_data = np.load(train_data_file)
+	train_X = train_data['inputs']
+	train_y = train_data['targets']
+
+	#Compute mean and range of X values in order to normalize training, validation and test
+	train_X_features_mean = np.zeros(train_X.shape)
+	train_X_features_range = np.zeros(train_X.shape)
+
+	for j in xrange(train_X.shape[1]):
+		train_X_features_mean[:, j] = float(np.mean(train_X[:, j]))
+		train_X_features_range[:, j]  = float((np.amax(train_X[:, j])-np.amin(train_X[:, j])))
+
+	#normalize the value of features to be between -1 to 1
+	for j in xrange(train_X.shape[1]):
+		train_X[:,j] = (train_X[:,j] - train_X_features_mean[0,j])/train_X_features_range[0,j]
+
+	#validation data
+	valid_data = np.load(valid_data_file)
+	valid_X = valid_data['inputs']
+	valid_y = valid_data['targets']
+
+	#normalize the value of features to be between -1 to 1
+	for j in xrange(valid_X.shape[1]):
+		valid_X[:,j] = (valid_X[:,j] - train_X_features_mean[0,j])/train_X_features_range[0,j]
+
+	#test data
+	test_data = np.load(test_data_file)
+	test_X = test_data['inputs']
+	test_y = test_data['targets']
+
+	#normalize the value of features to be between -1 to 1
+	for j in xrange(test_X.shape[1]):
+		test_X[:,j] = (test_X[:,j] - train_X_features_mean[0,j])/train_X_features_range[0,j]
+
+	# Run classifier
+	model = None
+	params = json.loads(params_json)
+
+	# Standard algorithms
+	if not ensemble_method:
+		# KNN
+		if model_name == 'knn':
+			model = KNNModel(params) # KNNModel({'k': 10})
+
+		# Logistic
+		elif model_name == 'logistic':
+			model = LogisticRegressionModel(params) # LogisticRegressionModel({'penalty': 'l2', 'regularization_term': 0.1})
+
+		# SVM
+		elif model_name == 'svm':
+			model = SVMModel(params) # SVMModel({'kernel': 'rbf', 'probability_flag': False})
+
+		# MoG
+		elif model_name == 'mog':
+			model = MOGModel(params) # MOGModel({'n_components': 20})
+
+		# Multi-layer perceptron (NNets)
+		elif model_name == 'neural_net':
+			model = NeuralNetworkModel(params) # NeuralNetworkModel({'xxx': 0})
+
+		# Decision tree
+		elif model_name == 'decision_tree':
+			model = DecisionTreeModel(params) # DecisionTreeModel({'criterion': 'gini', 'max_depth': 10, 'min_samples_split': 5})
+
+		#random forest
+		elif (model_name == "randomforest"):
+			model = RandomForestModel(params) # RandomForestModel({'n_estimators': 100, 'criterion': 'gini', 'max_depth': 3, 'min_samples_split': 5})
+
+		#extra trees
+		elif (model_name == "extratrees"):
+			model = ExtraTreesModel(params) # ExtraTreesModel({'n_estimators': 100, 'criterion': 'gini', 'max_depth': 3, 'min_samples_split': 5})
+
+		else:
+			raise NotImplementedError, "Invalid model name"
+
+	# Adaboost
+	elif (ensemble_method == 'adaboost'):
+		# Adaboost decision tree
+		if model_name == 'decision_tree':
+			model = AdaboostModel(params) # AdaboostModel({'algorithm_name': 'decision_tree', 'n_estimators': 25, 'criterion': 'gini', 'max_depth': 1, 'min_samples_split': 5})
+			# model_file = 'ada_dt_test_model.npz'
+			# train_output_file = 'examples-train-predictions-ada_dt-5000.npz'
+			# valid_output_file = 'examples-valid-predictions-ada_dt-5000.npz'
+
+		else:
+			raise NotImplementedError, "Invalid adaboost model"
+
+	# Bagging
+	elif (ensemble_method == "bagging"):
+		# Bagging decision tree
+		if model_name == 'decision_tree':
+			model = BaggingModel(params) # BaggingModel({'algorithm_name': 'decision_tree', 'n_estimators': 25, 'criterion': 'gini', 'max_depth': 3, 'min_samples_split': 5})
+			# model_file = 'bag_dt_test_model.npz'
+			# train_output_file = 'examples-train-predictions-bag_dt-5000.npz'
+			# valid_output_file = 'examples-valid-predictions-bag_dt-5000.npz'
+
+		elif model_name == 'logistic':
+			model = BaggingModel(params) # BaggingModel({'algorithm_name': 'logistic', 'n_estimators': 25, 'penalty': 'l2', 'regularization_term': 0.01})
+			# model_file = 'bag_log_test_model.npz'
+			# train_output_file = 'examples-train-predictions-bag_log-5000.npz'
+			# valid_output_file = 'examples-valid-predictions-bag_log-5000.npz'
+
+		else:
+			raise NotImplementedError, "Invalid bagging model"
+
+	# Create any necessary directories if needed
+	create_dir_if_not_exists(train_output_file)
+	create_dir_if_not_exists(valid_output_file)
+	create_dir_if_not_exists(test_output_file)
+	create_dir_if_not_exists(model_file)
+	create_dir_if_not_exists(output_file)
+
+	# Train
+	print('Start training...')
+	start = time.time()
+	model.train(train_X, train_y, model_file)
+	end = time.time()
+	train_time = end - start
+	print('Finished training...')
+
+	with open(output_file, 'w') as f:
+		f.write("SUPERPIXEL LEVEL RESULTS\n\n")
+		f.write("{0} - trained on {1}\n\n".format(model_name, train_data_file))
+		f.write("Training time (s): \n\t{0}\n\n".format(train_time))
+
+		# Predictions
+		f.write("===================\n")
+		f.write("TEST SET\n")
+		f.write("===================\n")
+		print('Start predicting on test...')
+		start = time.time()
+		test_pred = model.predict(test_X, model_file)
+		end = time.time()
+		predict_test_time = end - start
+		f.write("Testing prediction time (s):\n\t{0}\n".format(predict_test_time))
+		print('Finished predicting on testing...')
+
+		# Calculate CE/class rate on testing set
+		test_ce, test_class_rate, test_precision, test_recall, test_f1_score = model.evaluate(test_y, test_pred, cross_entropy_flag = True)
+		f.write("testing CE:\n\t{0}\n".format(test_ce))
+		f.write("testing classification rate:\n\t{0}\n".format(test_class_rate))
+		f.write("testing precision:\n\t{0}\n".format(test_precision))
+		f.write("testing recall:\n\t{0}\n".format(test_recall))
+		f.write("testing f1 score:\n\t{0}\n".format(test_f1_score))
+
+		# Also write to the summary file
+		fieldnames = ['algorithm', 'configuration', 'model_file', 'data_set', 'train_time', 'predict_time', 'ce', 'classification_rate', 'precision', 'recall', 'f1']
+		
+		if os.path.isfile(summary_file):
+			report = open(summary_file, 'a')
+			writer = csv.DictWriter(report, fieldnames=fieldnames)
+		else:
+			report = open(summary_file, 'w')
+			writer = csv.DictWriter(report, fieldnames=fieldnames)
+			writer.writeheader()
+		
+		# Write test results
+		writer.writerow({
+			'algorithm': model_name,
+			'configuration': params_json,
+			'model_file': model_file,
+			'data_set': 'test',
+			'train_time': train_time,
+			'predict_time': predict_test_time,
+			'ce': test_ce,
+			'classification_rate': test_class_rate,
+			'precision': test_precision,
+			'recall': test_recall,
+			'f1': test_f1_score
+		})
+
+		report.close()
+
+	# Save predictions
+	np.savez(test_output_file, predictions=test_pred)
--- run_main_extra_final.sh
+++ run_main_extra_final.sh
@@ -19,46 +19,47 @@ do
 	echo "----------------"
 	echo "${i} superpixels"
 	echo "----------------"
-	python main.py -m "${alg}" -e "${ensemble_method}" -o "${results_dir}/test_results/${ensemble_method}${alg}_${i}sp_superpixel_level.txt" -p "${params}"\
+	python final_main.py -m "${alg}" -e "${ensemble_method}" -o "${results_dir}/test_results/${ensemble_method}${alg}_${i}sp_superpixel_level.txt" -p "${params}"\
 		--train-data "superpixel_data/train_examples_${i}sp.npz" --train-predictions-output "${results_dir}/predictions/${ensemble_method}${alg}_${i}sp_train.npz" \
 		--valid-data "superpixel_data/valid_examples_${i}sp.npz" --valid-predictions-output "${results_dir}/predictions/${ensemble_method}${alg}_${i}sp_valid.npz" \
+		--test-data "superpixel_data/test_examples_${i}sp.npz" --test-predictions-output "${results_dir}/predictions/${ensemble_method}${alg}_${i}sp_test.npz" \
 		--model-file "${results_dir}/models/${ensemble_method}${alg}_${i}sp.npz" \
 		--summary-file "${run_dir}/superpixel_report_${start_date}.csv"
 
 	# Encode predictions and get pixel-level results
-	pixel_test_results_file="${results_dir}/test_results/${ensemble_method}${alg}_${i}sp_pixel_level.txt"
-	printf "PIXEL LEVEL RESULTS\n\n" > "${pixel_test_results_file}"
-	printf "===================\n" >> "${pixel_test_results_file}"
-	printf "TRAINING\n" >> "${pixel_test_results_file}"
-	printf "===================\n" >> "${pixel_test_results_file}"
+	# pixel_test_results_file="${results_dir}/test_results/${ensemble_method}${alg}_${i}sp_pixel_level.txt"
+	# printf "PIXEL LEVEL RESULTS\n\n" > "${pixel_test_results_file}"
+	# printf "===================\n" >> "${pixel_test_results_file}"
+	# printf "TRAINING\n" >> "${pixel_test_results_file}"
+	# printf "===================\n" >> "${pixel_test_results_file}"
 
-	echo "Encoding training predictions"
-	python encode_predictions.py \
-		-m "superpixel_data/train_superpixels_map_${i}sp.npz" \
-		-i "${results_dir}/predictions/${ensemble_method}${alg}_${i}sp_train.npz" \
-		-e "kit/data_road/training/divided_data/train/image_2" \
-		-o "${results_dir}/prediction_images/${i}sp/train/encoded" \
-		-ov "${results_dir}/prediction_images/${i}sp/train/encoded_overlay" >/dev/null 2>&1
-		# --generate-overlay \
+	# echo "Encoding training predictions"
+	# python encode_predictions.py \
+	# 	-m "superpixel_data/train_superpixels_map_${i}sp.npz" \
+	# 	-i "${results_dir}/predictions/${ensemble_method}${alg}_${i}sp_train.npz" \
+	# 	-e "kit/data_road/training/divided_data/train/image_2" \
+	# 	-o "${results_dir}/prediction_images/${i}sp/train/encoded" \
+	# 	-ov "${results_dir}/prediction_images/${i}sp/train/encoded_overlay" >/dev/null 2>&1
+	# 	# --generate-overlay \
 
-	python kit/devkit_road/python/evaluateRoad.py  "${results_dir}/prediction_images/${i}sp/train/encoded/" "kit/data_road/training/divided_data/train" "${run_dir}/pixel_report_${start_date}.csv" "${ensemble_method}${alg}" "${params}" "train" "${i}" >> "${pixel_test_results_file}"
+	# python kit/devkit_road/python/evaluateRoad.py  "${results_dir}/prediction_images/${i}sp/train/encoded/" "kit/data_road/training/divided_data/train" "${run_dir}/pixel_report_${start_date}.csv" "${ensemble_method}${alg}" "${params}" "train" "${i}" >> "${pixel_test_results_file}"
 
-	printf "\n===================\n" >> "${pixel_test_results_file}"
-	printf "VALID\n" >> "${pixel_test_results_file}"
-	printf "===================\n" >> "${pixel_test_results_file}"
+	# printf "\n===================\n" >> "${pixel_test_results_file}"
+	# printf "VALID\n" >> "${pixel_test_results_file}"
+	# printf "===================\n" >> "${pixel_test_results_file}"
 
-	echo "Encoding valid predictions"
-	python encode_predictions.py \
-		-m "superpixel_data/valid_superpixels_map_${i}sp.npz" \
-		-i "${results_dir}/predictions/${ensemble_method}${alg}_${i}sp_valid.npz" \
-		-e "kit/data_road/training/divided_data/valid/image_2" \
-		-o "${results_dir}/prediction_images/${i}sp/valid/encoded" \
-		-ov "${results_dir}/prediction_images/${i}sp/valid/encoded_overlay" >/dev/null 2>&1
-		# --generate-overlay \
+	# echo "Encoding valid predictions"
+	# python encode_predictions.py \
+	# 	-m "superpixel_data/valid_superpixels_map_${i}sp.npz" \
+	# 	-i "${results_dir}/predictions/${ensemble_method}${alg}_${i}sp_valid.npz" \
+	# 	-e "kit/data_road/training/divided_data/valid/image_2" \
+	# 	-o "${results_dir}/prediction_images/${i}sp/valid/encoded" \
+	# 	-ov "${results_dir}/prediction_images/${i}sp/valid/encoded_overlay" >/dev/null 2>&1
+	# 	# --generate-overlay \
 
-	python kit/devkit_road/python/evaluateRoad.py  "${results_dir}/prediction_images/${i}sp/valid/encoded/" "kit/data_road/training/divided_data/valid" "${run_dir}/pixel_report_${start_date}.csv" "${ensemble_method}${alg}" "${params}" "valid" "${i}" >> "${pixel_test_results_file}"
+	# python kit/devkit_road/python/evaluateRoad.py  "${results_dir}/prediction_images/${i}sp/valid/encoded/" "kit/data_road/training/divided_data/valid" "${run_dir}/pixel_report_${start_date}.csv" "${ensemble_method}${alg}" "${params}" "valid" "${i}" >> "${pixel_test_results_file}"
 
-	printf "\n===================\n" >> "${pixel_test_results_file}"
+	printf "\n===================\n" > "${pixel_test_results_file}"
 	printf "TESTING\n" >> "${pixel_test_results_file}"
 	printf "===================\n" >> "${pixel_test_results_file}"
 
